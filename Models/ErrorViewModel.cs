b13eb37f6de188f9bd5e1ccba2e21f9c716bffde
Juan Pablo Henao <juan.henaoe1@gmail.com>
2022-10-26 20:55:41 -0500

    subir avances a nube

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Manejopresupuestal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
